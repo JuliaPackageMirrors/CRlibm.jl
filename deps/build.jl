37e07fe985714c52e850e105076b2975df803db0
David Sanders <dpsanders@gmail.com>
2016-05-04 09:06:07 -0400

    Move build step to build function, don't run on Windows

--- deps/build.jl
+++ deps/build.jl
@@ -6,38 +6,43 @@
 
 suffix = ""
 
+function build()
+    lib_name = "crlibm-1.0beta4"
+
+    src_dir = "src"
+    cd(src_dir)
+
+    file = "http://lipforge.ens-lyon.fr/frs/download.php/162/$(lib_name).tar.gz"
+
+    println("Downloading the library files from $file")
+    println("Working in ", pwd())
+
+
+    run(`$(my_download) $file`)
+    #download(file)
+    run(`tar xzf $(lib_name).tar.gz`)
+
+    #srcdir = "$(src_dir)/$(lib_name)"
+
+    cd(lib_name)
+    println("Working in ", pwd())
+
+    suffix = @osx? "dylib" : "so"
+    run(`./configure CFLAGS=-fpic --silent`)
+    println("Working in ", pwd())
+
+    run(`make -s V=0`)
+    run(`make -s -f ../shared.mk SUFFIX=$suffix V=0`)
+end
+
+
 @unix_only begin
     my_download = @osx ? `curl -O` : `wget`
     suffix = @osx ? "dylib" : "so"
+
+    build()
 end
 
 @windows_only begin
-    error("Package not currently available on Windows")
+    info("CRlibm will currently just use an MPFR wrapper alternative on Windows)
 end
-
-lib_name = "crlibm-1.0beta4"
-
-src_dir = "src"
-cd(src_dir)
-
-file = "http://lipforge.ens-lyon.fr/frs/download.php/162/$(lib_name).tar.gz"
-
-println("Downloading the library files from $file")
-println("Working in ", pwd())
-
-
-run(`$(my_download) $file`)
-#download(file)
-run(`tar xzf $(lib_name).tar.gz`)
-
-#srcdir = "$(src_dir)/$(lib_name)"
-
-cd(lib_name)
-println("Working in ", pwd())
-
-suffix = @osx? "dylib" : "so"
-run(`./configure CFLAGS=-fpic --silent`)
-println("Working in ", pwd())
-
-run(`make -s V=0`)
-run(`make -s -f ../shared.mk SUFFIX=$suffix V=0`)
